04ece97962f27cc57af3fe0c098543b2ea24892c
Mich <mejia.cambara.michelle@gmail.com>
2025-02-04 21:27:01 -0600

    Creacion de variables con prompt

---
+++ index.js
@@ -0,0 +1,33 @@
+
+/*console.log('Hola desde JavaScript');
+
+
+var nombre = prompt('Ingresa tu nombre');
+var edad = prompt('Ingresa tu edad');
+var gusto1 = prompt('Ingresa tu pasatiempo favorito');
+var gusto2 = prompt('Ingresa tu otro pasatiempo favorito');
+
+//El metodo typeof nos dice el tipo de dato de la variable
+//console.log(typeof edad);
+
+//Concatenar 
+console.log('Mi nombre es:', nombre,'tengo la edad de:', edad, 'me gusta', gusto1, 'y tambien', gusto2);
+// Ctrl + / sirve para comentar
+console.log(`Mi nombre es: ${nombre}, tengo la edad de ${edad} y me gusta ${gusto1} y tambien ${gusto2}`)
+
+// Metodos de salida
+//console.log
+//alert
+//document.write
+
+// Metodos de entrada
+//prompt
+*/
+//Prompt por default devuelve valores string 
+var numero1 = parseInt(prompt('Ingresa el primer dato'));
+var numero2 = parseInt(prompt('Ingresa el segundo dato'));
+var numero3 = parseInt(prompt('Ingresa el tercer dato'));
+
+console.log(numero1 + numero2 + numero3);
+//Otra forma de hacerlo
+//console.log(number(numero1) + number(numero2) + number(numero3));
